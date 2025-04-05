5e17fb7d2d9b057ad59dddd1b5cf6b3e19653c85
KnownNight <kasich.nszar@gmail.com>
2025-04-05 20:46:28 +0300

    initial commit

---
+++ CobainStats/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CobainStats.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
